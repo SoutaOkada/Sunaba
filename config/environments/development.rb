7047da9d8f7ac8d03d203442671839f3f567db4d
SoutaOkada <soscmkao0926@gmail.com>
2022-05-09 16:38:13 +0000

    [update] update host authority
    add sentence for host authority in config/environments/development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "a5e96d1779a34470a206768732b68eac.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  
 end

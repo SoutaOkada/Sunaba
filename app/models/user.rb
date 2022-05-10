a5d69206d8ce228ff8554d4b3c4ee949a9b1fef0
SoutaOkada <soscmkao0926@gmail.com>
2022-05-10 14:23:44 +0000

    [add] association to user
    add association to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :posts
+  has_many :comments
+  has_many :relationships
+  has_many :follow_games
+  has_many :post_favs
+  has_many :comment_favs
+
 end

d64a644021edf37cbadf93053d467e16a9700dfc
SoutaOkada <soscmkao0926@gmail.com>
2022-05-10 14:10:35 +0000

    [add] add comments columns
    add columns to comments migration file
    
    exe db:migrate

--- db/migrate/20220510140418_create_post_favs.rb
+++ db/migrate/20220510140418_create_post_favs.rb
@@ -1,6 +1,9 @@
 class CreatePostFavs < ActiveRecord::Migration[6.1]
   def change
     create_table :post_favs do |t|
+      
+      t.string :text
+      t.boolean :is_active, null: false, default: true
 
       t.timestamps
     end

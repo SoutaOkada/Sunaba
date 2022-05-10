7ef655acbf20a6c7735611291e515757469f997a
SoutaOkada <soscmkao0926@gmail.com>
2022-05-10 13:49:54 +0000

    [add] add game_links columns
    add columns to game_links migration file
    exe db:migrate

--- db/migrate/20220510134150_create_game_links.rb
+++ db/migrate/20220510134150_create_game_links.rb
@@ -1,6 +1,9 @@
 class CreateGameLinks < ActiveRecord::Migration[6.1]
   def change
     create_table :game_links do |t|
+      
+      t.string :link_title
+      t.string :link
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_10_133331) do
+ActiveRecord::Schema.define(version: 2022_05_10_134150) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 2022_05_10_133331) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "game_links", force: :cascade do |t|
+    t.string "link_title"
+    t.string "link"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "games", force: :cascade do |t|

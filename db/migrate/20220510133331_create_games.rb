5746da739b2b8b04b83e29a7035eabd2e36723be
SoutaOkada <soscmkao0926@gmail.com>
2022-05-10 13:40:49 +0000

    [add] add game columns
    add columns to games migration file
    exe db:migrate

--- db/migrate/20220510133331_create_games.rb
+++ db/migrate/20220510133331_create_games.rb
@@ -2,6 +2,9 @@ class CreateGames < ActiveRecord::Migration[6.1]
   def change
     create_table :games do |t|
 
+      t.string :game_title
+      t.boolean :is_active, null: false, default: true
+
       t.timestamps
     end
   end

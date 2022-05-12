3e5501cfc7214ce8e1a48c5aaba72b681543bc46
SoutaOkada <soscmkao0926@gmail.com>
2022-05-12 04:16:02 +0000

    [add] create admin controller
    exe rails g controller admin/homes top
    edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+
   devise_for :admins
   devise_for :users
+
+  namespace :admin do
+    get 'homes/top'
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

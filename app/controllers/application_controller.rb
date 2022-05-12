b82e5f0970304569e4a3f5b74496aa0545beb1a4
SoutaOkada <soscmkao0926@gmail.com>
2022-05-12 12:56:08 +0000

    [update] set after sign_in & sign_out, redistration pathes
    edit application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(resource)
+    if current_user
+      root_path
+    else
+      admin_homes_top_path
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    if resource == :admin
+      new_admin_session_path
+    else
+      root_path
+    end
+  end
+
 end

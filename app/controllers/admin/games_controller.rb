97425f944a419974ab72a664b85075294392641f
SoutaOkada <soscmkao0926@gmail.com>
2022-05-14 03:19:27 +0000

    [update] add post new game form in games/index
    edit admin/games/index.html & games controller

--- app/controllers/admin/games_controller.rb
+++ app/controllers/admin/games_controller.rb
@@ -1,11 +1,15 @@
 class Admin::GamesController < ApplicationController
   def index
+    @game = Game.new
   end
 
   def new
   end
 
   def create
+    @game = Game.new(game_params)
+    @game.save
+    redirect_to admin_games_path
   end
 
   def show
@@ -15,5 +19,11 @@ class Admin::GamesController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:game_title)
   end
 end

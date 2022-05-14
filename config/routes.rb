a7786e4aed81103ab570364dec0e2921490dd7b2
SoutaOkada <soscmkao0926@gmail.com>
2022-05-14 02:30:05 +0000

    [add] add admin/games controller & views, routes
    exe rails g controller admin/games index new create show edit update
    edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+
   devise_for :admins, controllers: {
       sessions: 'admin/sessions'
   }
@@ -18,6 +19,9 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get 'homes/top'
+
+    resources :games, only: [:index, :new, :create, :show, :edit, :update]
+
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

88ea6bd301b8c6fc8b1a28e2a7fc336d4cffdf13
SoutaOkada <soscmkao0926@gmail.com>
2022-05-15 15:38:18 +0000

    [update] add withdrawal from admin authority feature
    edit admin/users/show.html & users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,5 +8,14 @@ class Admin::UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to request.referer
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:is_active)
   end
 end

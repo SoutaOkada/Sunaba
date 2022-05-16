f7881e870dfea438e5fe83ca92dbf51486058bf4
SoutaOkada <soscmkao0926@gmail.com>
2022-05-16 15:08:21 +0000

    [update] add edit user profile feature
    edit public/users_controller & users/edit.html

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -7,14 +7,25 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(params[:id])
   end
 
   def confirm
   end
 
   def withdrawal
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name, :gender, :open_gender, :nickname, :self_intro, \
+      :image)
   end
 end

605656d49dadcf291be8ab300579cb3bc06c46c4
SoutaOkada <soscmkao0926@gmail.com>
2022-05-16 11:28:26 +0000

    [update] limit access to admin pages from not admins
    edit application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_admin!, if: :admin_url
 
   def after_sign_in_path_for(resource)
     if current_user
@@ -15,6 +16,10 @@ class ApplicationController < ActionController::Base
     else
       root_path
     end
+  end
+
+  def admin_url
+    request.fullpath.include?("/admin")
   end
 
   protected

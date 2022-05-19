a05d92e0980497fd7cbf0932990a00ccda7f4c14
SoutaOkada <soscmkao0926@gmail.com>
2022-05-19 02:55:42 +0000

    [update] add post comment feature
    edit public/posts & comments controller & posts/show/.html

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,7 +1,16 @@
 class Public::CommentsController < ApplicationController
   def create
+    @comment = Comment.new(comment_params)
+    @comment.save
+    redirect_to post_path(@comment.post.id)
   end
 
   def update
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:post_id, :user_id, :text)
   end
 end

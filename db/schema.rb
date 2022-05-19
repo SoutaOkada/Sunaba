839c7a53a1b0ce6a415a0cab29a39bfa2da8cd06
SoutaOkada <soscmkao0926@gmail.com>
2022-05-19 02:26:25 +0000

    [add] add columns to comments model
    exe rails g migration add_column_to_comments
    edit migration file
    exe db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_19_020728) do
+ActiveRecord::Schema.define(version: 2022_05_19_022240) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -62,6 +62,8 @@ ActiveRecord::Schema.define(version: 2022_05_19_020728) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.integer "post_id"
+    t.string "text"
+    t.boolean "is_active"
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end

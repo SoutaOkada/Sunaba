8573981624ef6af88b2d73adb3e6dba2805bd752
SoutaOkada <soscmkao0926@gmail.com>
2022-05-19 17:06:04 +0000

    [add] add admin posts controller
    exe rails g controller admin/posts show update
    edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,8 @@ Rails.application.routes.draw do
 
     resources :users, only: [:index, :show, :update]
 
+    resources :posts, only: [:show, :update]
+
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

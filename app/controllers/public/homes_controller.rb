60f7204fe360b0de722fc7babfc079c8b3754726
SoutaOkada <soscmkao0926@gmail.com>
2022-05-23 13:02:32 +0000

    [update] update homes page
    edit public/homes controller & homes/top.html

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,4 +1,11 @@
 class Public::HomesController < ApplicationController
   def top
+    @posts = Post.where(is_active: true)
+    @games = Game.where(is_active: true)
+
+    if user_signed_in?
+      @user_posts = Post.where(user_id: [current_user.following_ids], is_active: true).order(created_at: :DESC)
+      @game_posts = Post.where(game_id: [current_user.game_ids]).order(created_at: :DESC)
+    end
   end
 end

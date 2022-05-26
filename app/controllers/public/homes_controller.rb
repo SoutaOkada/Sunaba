69813e2de2ae246b4f00d9da6277d489f1c211ca
SoutaOkada <soscmkao0926@gmail.com>
2022-05-26 21:20:35 +0000

    [update] update layouts
    edit views & controllers

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,11 +1,11 @@
 class Public::HomesController < ApplicationController
   def top
-    @posts = Post.where(is_active: true).limit(30).order(created_at: :DESC)
+    @posts = Post.where(is_active: true).limit(13).order(created_at: :DESC)
     @games = Game.where(is_active: true).limit(30).order(created_at: :DESC)
 
     if user_signed_in?
-      @user_posts = Post.where(user_id: [current_user.following_ids], is_active: true).limit(30).order(created_at: :DESC)
-      @game_posts = Post.where(game_id: [current_user.game_ids], is_active: true).limit(30).order(created_at: :DESC)
+      @user_posts = Post.where(user_id: [current_user.following_ids], is_active: true).limit(6).order(created_at: :DESC)
+      @game_posts = Post.where(game_id: [current_user.game_ids], is_active: true).limit(6).order(created_at: :DESC)
     end
   end
 end
